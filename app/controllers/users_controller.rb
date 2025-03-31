a8b2e867208c6b42506b9a1669400ef970a94c22
joshua-white0507 <joshua.white@solirius.com>
2025-03-31 15:32:15 +0100

    First MVP finished now adding Bootstrap

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
+ 
+    def new
+        @user = User.new()
+    end
+ 
+    def create
+        @user = User.new(user_params)
+       if @user.save
+           redirect_to root_url
+       else
+           render :new
+       end
+    end
+
+    def destroy
+        User.find(params[:id]).destroy
+        redirect_to root_url
+    end
+
+
+    private
+
+   def user_params
+       params.require(:user).permit(:name, :practice)
+  
+   end
+
+
+end
