ab58a0044998d0bce8f358e7f46b3f137ff910e4
joshua-white0507 <joshua.white@solirius.com>
2025-04-02 11:00:01 +0100

    added in a profile picture for the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
+         has_one_attached :profile_photo
 validates :password, confirmation: true
 validates :email, presence: true, uniqueness: true
 validates :experience, length: { maximum: 1000 }, allow_blank: true
